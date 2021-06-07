c2f26f2322100100295dfa0d9fa6edc179fa7120
Niels Justesen <njustesen@gmail.com>
2021-06-07 21:08:45 +0200

    terran

--- starcraft/gfx/app.py
+++ starcraft/gfx/app.py
@@ -39,10 +39,21 @@ def num_products(item):
 
 
 @app.route('/protoss', methods=['GET', 'POST'])
-def my_form_post():
+def protoss():
     parsed_yaml_file = yaml.load(open('data/starcraft.yaml', 'r'), Loader=yaml.FullLoader)
     return render_template('templates/index.html',
                            race='Protoss',
+                           data=parsed_yaml_file,
+                           img=img,
+                           num_products=num_products,
+                           level_requirements=level_requirements)
+
+
+@app.route('/terran', methods=['GET', 'POST'])
+def terran():
+    parsed_yaml_file = yaml.load(open('data/starcraft.yaml', 'r'), Loader=yaml.FullLoader)
+    return render_template('templates/index.html',
+                           race='Terran',
                            data=parsed_yaml_file,
                            img=img,
                            num_products=num_products,
